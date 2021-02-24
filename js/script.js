e0b0cd68323a0171bbc2155c8731573b3641e1ac
Gael Blanchemain <anegg0@pop-os.localdomain>
2021-02-24 11:18:30 -0800

    focus operational

---
+++ js/script.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log('pouet');
+
+/* Variable to store form inputs -  */
+// const form = document.querySelector("form");
+const nameElement = document.querySelector("#name");
+nameElement.focus();
+// const email = document.querySelector("#email");
+// const languagesBox = document.querySelector('#languages-box');
+// const languageTotalElement = document.querySelector('#language-total');
+// let languageTotal = 0;
