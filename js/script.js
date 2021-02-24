70fac828498d697fa06cc240f0059fd8a33236d2
Gael Blanchemain <anegg0@pop-os.localdomain>
2021-02-24 14:52:14 -0800

    job other function operational

--- js/script.js
+++ js/script.js
@@ -1,11 +1,37 @@
 "use strict";
-console.log('pouet');
 
 /* Variable to store form inputs -  */
 // const form = document.querySelector("form");
 const nameElement = document.querySelector("#name");
 nameElement.focus();
 // const email = document.querySelector("#email");
+// const email = document.querySelector("#email");
 // const languagesBox = document.querySelector('#languages-box');
 // const languageTotalElement = document.querySelector('#language-total');
 // let languageTotal = 0;
+
+const jobRole = document.querySelector(".title");
+const jobOther = document.querySelector("#other-job-role");
+jobOther.style.visibility = 'hidden';
+const selectorJobs = document.querySelector("#title")
+const allJobs = document.querySelectorAll('#job')
+const selectorDesigns = document.querySelector('#design')
+const selectorColor = document.querySelector('#color')
+selectorColor.style.visibility = 'hidden';
+const colorDiv = document.querySelector('#shirt-colors')
+
+selectorJobs.addEventListener('change', e => {
+console.log(e.target.value)
+
+if (e.target.value == 'other') {
+jobOther.style.visibility = 'visible';
+}
+})
+
+selectorDesigns.addEventListener('change', e => {
+console.log(e.target.value)
+
+if (e.target.value !== 'js puns')
+selectorColor.style.visibility = 'visible';
+}
+)
