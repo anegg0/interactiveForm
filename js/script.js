2d15620b5a17985fd56e9707a5282a8f931ee6c8
Gael Blanchemain <anegg0@pop-os.localdomain>
2021-02-25 19:03:06 -0800

    prevent concurrent bookings

--- js/script.js
+++ js/script.js
@@ -12,51 +12,54 @@ nameElement.focus();
 
 const jobRole = document.querySelector(".title");
 const jobOther = document.querySelector("#other-job-role");
-jobOther.style.visibility = 'hidden';
-const selectorJobs = document.querySelector("#title")
-const allJobs = document.querySelectorAll('#job')
-let selectorDesigns = document.querySelector('#design')
-const selectorColor = document.querySelector('#color')
+jobOther.style.visibility = "hidden";
+const selectorJobs = document.querySelector("#title");
+const allJobs = document.querySelectorAll("#job");
+
+selectorJobs.addEventListener("change", (e) => {
+  if (e.target.value == "other") {
+    jobOther.style.visibility = "visible";
+  }
+});
+
+let selectorDesigns = document.querySelector("#design");
+const selectorColor = document.querySelector("#color");
 const colors = selectorColor.children;
 console.log(colors);
-const colorDiv = document.querySelector('#shirt-colors')
-colorDiv.style.visibility = 'hidden';
+const colorDiv = document.querySelector("#shirt-colors");
+colorDiv.style.visibility = "hidden";
 
-selectorJobs.addEventListener('change', e => {
-
-if (e.target.value == 'other') {
-jobOther.style.visibility = 'visible';
-}
-})
-
-selectorDesigns.addEventListener('change', e => {
+selectorDesigns.addEventListener("change", (e) => {
   const jsPuns = document.querySelectorAll('[data-theme="js puns"]');
   const heartJS = document.querySelectorAll('[data-theme="heart js"]');
 
- if (e.target.value == "js puns"){
-        
-for (let i = 0; i < jsPuns.length; i++) {
-
-colorDiv.style.visibility = 'visible'
- jsPuns[i].style.visibility = 'visible';
- heartJS[i].style.visibility = 'hidden';
-//  console.log(jsPuns[i].value)
- console.log(jsPuns.length)
-
-
+  if (e.target.value == "js puns") {
+    for (let i = 0; i < jsPuns.length; i++) {
+      colorDiv.style.visibility = "visible";
+      jsPuns[i].style.visibility = "visible";
+      heartJS[i].style.visibility = "hidden";
     }
-// console.log(e.target.value);
-// console.log(selectorColor.length)
+  } else if (e.target.value == "heart js") {
+    for (let i = 0; i < heartJS.length; i++) {
+      colorDiv.style.visibility = "visible";
+      heartJS[i].style.visibility = "visible";
+      jsPuns[i].style.visibility = "hidden";
+    }
+  }
+});
 
-    } else {
+const registerActivities = document.querySelector("#activities-box");
 
-for (let i = 0; i < heartJS.length; i++) {
-
-colorDiv.style.visibility = 'visible'
- heartJS[i].style.visibility = 'visible';
- jsPuns[i].style.visibility = 'hidden';
- console.log(heartJS[i].value)
-}    
-}
-}
-)
+registerActivities.addEventListener("change", (e) => {
+  const clicked = e.target;
+  const clickedTypeTime = clicked.getAttribute("data-day-and-time");
+  const activities = registerActivities.children;
+  // console.log(activities[2].children[2].innerHTML);
+  // console.log(activities[2].getAttribute('data-day-and-time'))
+  for (let i = 0; i < activities.length; i++) {
+    if (clickedTypeTime == activities[i].children[2].innerHTML) {
+      console.log(clickedTypeTime);
+      activities[i].children[0].disabled = "true";
+    }
+  }
+});
